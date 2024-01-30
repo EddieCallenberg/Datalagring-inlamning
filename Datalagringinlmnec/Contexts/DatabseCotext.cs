78b46aca9cb4ea19c1ac0adb0be0f8be2c861d9f
Eddie <callenberg99@gmail.com>
2024-01-30 10:20:10 +0100

    Första migration gjort samt uppdaterat databasen med den migrationen. Även gjro tså att git ser databsfilerna.

---
+++ Datalagringinlmnec/Contexts/DatabseCotext.cs
@@ -0,0 +1,17 @@
+﻿using Datalagringinlmnec.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datalagringinlmnec.Contexts;
+
+internal class DatabseCotext : DbContext
+{
+    public DatabseCotext(DbContextOptions<DatabseCotext> options) : base(options)
+    {
+    }
+
+    public DbSet<AdressEntity> Adresses { get; set; }
+    public DbSet<CategoryEntity> Categories { get; set; }
+    public DbSet<CustomerEntity> Customers { get; set; }
+    public DbSet<ProductEntity> Products { get; set; }
+    public DbSet<RoleEntity> Roles { get; set; }
+}
--- Datalagringinlmnec/Program.cs
+++ Datalagringinlmnec/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using Datalagringinlmnec.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
 {
+
     private static void Main(string[] args)
     {
-        
+        var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
+        {
+
+            services.AddDbContext<DatabseCotext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\CSharp-projects-school\Datalagringinlmnec\Datalagringinlmnec\Data\database.mdf;Integrated Security=True;Connect Timeout=30"));
+
+        }).Build();
     }
 }
