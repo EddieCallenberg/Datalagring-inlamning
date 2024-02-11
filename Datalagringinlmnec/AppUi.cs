22188d409aec1d54a3d8af1e2b18c6c9a4c5ba9c
Eddie <callenberg99@gmail.com>
2024-02-11 10:59:26 +0100

    Allt för G klart, tror jag.

---
+++ Datalagringinlmnec/AppUi.cs
@@ -0,0 +1,48 @@
+﻿using Datalagringinlmnec.Services;
+
+namespace Datalagringinlmnec;
+
+internal class AppUi
+{
+    private readonly ProductService _productService;
+
+    public AppUi(ProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public void CreateProduct_Ui()
+    {
+        Console.Clear();
+        Console.WriteLine("Create Product");
+
+        Console.WriteLine("Product title: ");
+        var newProductTitle = Console.ReadLine()!;
+
+        Console.WriteLine("Product Price: ");
+        decimal newProductPrice = decimal.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Product Category: ");
+        var newProductCategory = Console.ReadLine()!;
+
+        var result =_productService.CreateProduct(newProductTitle, newProductPrice, newProductCategory);
+        if (result != null)
+        {
+            Console.Clear();
+            Console.WriteLine("New product was created.");
+            Console.ReadKey();
+        }
+    }
+
+    public void showProducts_Ui()
+    {
+        Console.Clear();
+        var products = _productService.GetAllProduct();
+        foreach (var product in products)
+        {
+            Console.WriteLine(product.ProductName);
+            Console.WriteLine(product.ProductPrice);
+            Console.WriteLine(product.Category.CategoryName);
+        }
+    }
+}
--- Datalagringinlmnec/Program.cs
+++ Datalagringinlmnec/Program.cs
@@ -1,4 +1,7 @@
-﻿using Datalagringinlmnec.Contexts;
+﻿using Datalagringinlmnec;
+using Datalagringinlmnec.Contexts;
+using Datalagringinlmnec.Repositories;
+using Datalagringinlmnec.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,9 +13,34 @@ internal class Program
     {
         var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
         {
-
             services.AddDbContext<DatabseCotext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\CSharp-projects-school\Datalagringinlmnec\Datalagringinlmnec\Data\database.mdf;Integrated Security=True;Connect Timeout=30"));
-
+            services.AddScoped<AdressRepository>();
+            services.AddScoped<CategoryRepository>();
+            services.AddScoped<RoleRepository>();
+            services.AddScoped<ProductRepository>();
+            services.AddScoped<CustomerRepository>();
+            services.AddScoped<AdressService>();
+            services.AddScoped<CategoryService>();
+            services.AddScoped<RoleService>();
+            services.AddScoped<ProductService>();
+            services.AddScoped<CustomerService>();
+            services.AddSingleton<AppUi>();
         }).Build();
+
+        var appUi = builder.Services.GetRequiredService<AppUi>();
+        Console.WriteLine("1. Create new product");
+        Console.WriteLine("2. Show all products");
+        int switchCaseSelector = int.Parse(Console.ReadLine()!);
+        switch (switchCaseSelector)
+        {
+            case 1:
+                appUi.CreateProduct_Ui();
+                break;
+            case 2:
+                appUi.showProducts_Ui();
+                break;
+
+        }
+        
     }
 }
